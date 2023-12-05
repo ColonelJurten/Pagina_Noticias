6e00c2d0de59ed7fbee5d7e3ae879a1b1d731e03
DOOMslayer616 <albscjr.03@gmail.com>
2023-12-05 05:19:25 -0600

    Actualización

---
+++ bd/conexion.js
@@ -0,0 +1,17 @@
+var admin = require("firebase-admin");
+var keys = require("../keys-8a2a5-firebase-adminsdk-2jbjs-c45ffa86d8.json");
+
+admin.initializeApp({
+    credential:admin.credential.cert(keys)
+});
+
+var db=admin.firestore();
+var conexionUsuarios=db.collection("Keys1");
+var conexionProductos = db.collection("productos");
+var conexionNoticias=db.collection("noticias");
+
+module.exports = {
+    conexionUsuarios,
+    conexionProductos,
+    conexionNoticias
+};
